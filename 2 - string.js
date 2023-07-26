e06cefe72d6a15f3458ffd3da2535912f654cc2c
kreby455 <clebinhoalves2011@gmail.com>
2023-07-26 06:29:43 -0300

    Estudo strings

---
+++ 2 - string.js
@@ -0,0 +1,9 @@
+/*
+1. String -> Textos.
+*/
+
+const texto1 = "textos com aspa duplas"
+const texto2 = 'textos com aspa simples'
+const texto3 =`textos com crase` // posso pular quantas linhas eu quiser
+
+console.log(texto1)
