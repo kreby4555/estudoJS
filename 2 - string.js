0453ce2010957df648aa234d9fff33bd42146759
kreby455 <clebinhoalves2011@gmail.com>
2023-07-26 06:30:38 -0300

    Strings

--- 2 - string.js
+++ 2 - string.js
@@ -4,6 +4,6 @@
 
 const texto1 = "textos com aspa duplas"
 const texto2 = 'textos com aspa simples'
-const texto3 =`textos com crase` // posso pular quantas linhas eu quiser
+const texto3 =`textos com crase` // posso pular quantas linhas eu quiser/ Template literals ou Template String
 
 console.log(texto1)
