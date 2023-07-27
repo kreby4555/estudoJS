32f336a641f4e6ac8473fa048b14886ac6f20708
kreby455 <clebinhoalves2011@gmail.com>
2023-07-27 05:25:33 -0300

    Eventos

--- Web/script.js
+++ Web/script.js
@@ -1,31 +1,17 @@
 /*
+Eventos
 
-document -> HTML
-
-getElementById -> Trás UM elemento pelo ID
-getElementByClassName -> Trás TODOS os elementos com essa classe
-getElementByTagNAme -> Trás TODOS os elementos com essa TAG
-getElementByName -> Trás TODOS os elementos com esse NAME
-
-querySelector -> Trás UM elemento, o PRIMEIRO que encontrar
-querySelectAll -> Trás TODOS os elementos que encontrar
-
-todos usao aspas duplas
-
-Alterando e Acessando textos
-
-    textContent -> Pega todo o conteúdo
-    innerText -> Pega APENAS o texto
-    innerHTML -> Permite adcionar HTML e texto
 */
 
-const elements = document.querySelector(".paragraph-js")
+const input = document.querySelector("#main-input")
+const input2 = document.querySelector(".add")
 
-console.log(elements.textContent) // SÓ HTML
-console.log(elements.innerText) // LEVA EM CONTA O CSS
-console.log(elements.innerHTML) // TRÁS TUDO -> Permite adicicionar HTML
 
-const button = document.querySelector("h1")
+function cliqueiNoBotao() {
+    input2.innerHTML = input.value
+}
 
-button.style.backgroundColor = "red"
-button.style.fontSize = "33px"
+function digiteiNoInput() {
+    console.log(input.value)
+}
+    
